97c84e287d2afb73ae6e8b187401aab744d534e7
vivianlink <vvivian@sfu.ca>
2016-11-17 11:28:35 -0800

    comment section

--- app/controllers/prof_controller.rb
+++ app/controllers/prof_controller.rb
@@ -1,5 +1,13 @@
 class ProfController < ApplicationController
   def index
+  	@profs = Course.search("",
+        "","","", "", params[:prof_name], "", "")
+
+  	# get unique instructor names
+  	@profs = @profs.uniq.pluck(:instructor) 
+  	
+  	# sort
+  	@profs = @profs.sort! 
   end
 
   def list
