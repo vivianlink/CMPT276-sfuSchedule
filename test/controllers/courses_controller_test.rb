ba71c0d22577998958c6f9b09daf59a229113b77
vpeng <vpeng@sfu.ca>
2016-11-17 13:30:27 -0800

    Added tests "Only Show and Back accessible for non admin users"

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -4,4 +4,36 @@ class CoursesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Only Show and Back accessible for non admin users" do
+    get :index
+    assert_response :success
+    assert_select "td", {count: Course.all.count, text: "Show"}
+    assert_select "td", {count: 0, text: "Edit"}
+    assert_select "td", {count: 0, text: "Destroy"}
+    assert_select "a[href=?]", new_course_path, {count: 0, text: "New course"}
+    assert_select "a[href=?]", welcome_index_path, {count: 1, text: "Back"}
+
+    session[:user_name] = 1
+    session[:user_id] = 1
+    get :index
+    assert_response :success
+    assert_select "td", {count: Course.all.count, text: "Show"}
+    assert_select "td", {count: 0, text: "Edit"}
+    assert_select "td", {count: 0, text: "Destroy"}
+    assert_select "a[href=?]", new_course_path, {count: 0, text: "New course"}
+    assert_select "a[href=?]", welcome_index_path, {count: 1, text: "Back"}
+
+    session[:is_admin] = true
+    get :index
+    assert_response :success
+    assert_select "td", {count: Course.all.count, text: "Show"}
+    assert_select "td", {count: Course.all.count, text: "Edit"}
+    assert_select "td", {count: Course.all.count, text: "Destroy"}
+    assert_select "a[href=?]", new_course_path, {count: 1, text: "New course"}
+    assert_select "a[href=?]", welcome_index_path, {count: 1, text: "Back"}
+
+    session[:user_name] = nil
+    session[:user_id] = nil
+    session[:is_admin] = false
+  end
 end
