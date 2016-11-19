a627cdf51a7352b144f6ba3b802c3ce3c8731fc6
vivianlink <vvivian@sfu.ca>
2016-11-18 19:08:40 -0800

    some tests

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,7 @@
 class Rating < ActiveRecord::Base
+	 validates_length_of :prof_name, :maximum => 30
+	 validates_length_of :prof_name, :minimum => 3
+	 validates :user_name, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+
+	 validates :rating, numericality: { only_integer: true } 
 end
--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -3,5 +3,38 @@ require 'test_helper'
 class RatingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
-  # end
+  # end   test "test username	with invalid vaule" do
+
+    test "test prof for legitimate name" do
+		r=Rating.new('comment'=>'good prof, awesome!', 'rating'=> 9 , 
+					'prof_name'=>'Mary', 'user_name'=>'vivian')
+		assert  r.valid?, "All good"
+
+		r.prof_name = 'asdfghjkl sdfghjk sdfghjk sdfghjk sdfghj sdfghj sdfghj dfghjk dfghjk dfghjk '
+		assert_not  r.valid?, "Professor name can have maximum of 30 letters"
+
+		r.prof_name = 'hi'
+		assert_not  r.valid?, "Professor name can't be too short, minimum 3 letters"
+
+	end 
+
+	test "test for legitimate user name" do
+		r1=Rating.new('comment'=>'good prof, awesome!', 'rating'=> 9, 
+					'prof_name'=>'Mary', 'user_name'=>'vi')
+		assert_not  r1.valid?, "the user_name is too short, need min 3 characters"
+
+		r1.user_name = 'asdfghjkl sdfghjk sdfghjk sdfghjk sdfghj sdfghj sdfghj df'
+		assert_not  r1.valid?, "user_name is too long, max 20 characters"
+		
+	end
+
+	test "rating must only be numeric" do
+		r2=Rating.new('comment'=>'good prof, awesome!', 'rating'=>'testest', 
+					'prof_name'=>'Mary', 'user_name'=>'vi')
+		assert_not  r2.valid?, "rating need to be integer"
+
+		r2.rating = 0.88
+		assert_not  r2.valid?, "rating must be integer"
+	end
+
 end
