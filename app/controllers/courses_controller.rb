63a7485368316aeb60db11d4392af5d938dd2e22
jun <kzh36g@gmail.com>
2016-11-19 22:18:55 -0800

    index will not show section and prof etc

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,19 @@
 class CoursesController < ApplicationController
 
+ helper_method :sort_column, :sort_direction
+  
+    #
 
 	def index
-      @courses = Course.all
-
+      #@courses = Course.all
+      @courses = Course.order(sort_column + " " + sort_direction)
       #for search
-      @courses = Course.search(params[:faculty],
-        params[:number],params[:year],params[:semester], params[:section], params[:instructor], params[:unit], params[:designation])
-  
+      @courses = Course.search(params[:faculty], params[:number],params[:year], 
+        params[:semester],  params[:unit],  params[:designation] )
+
+
+       # params[:section], params[:instructor], params[:unit], 
+       # params[:designation])
   	end
 
 	def show
@@ -24,13 +30,14 @@ class CoursesController < ApplicationController
   end  
 
 
-	def update
+
+  def update
     @course = Course.find(params[:id])
 
-    if @course.update(course_params)
-     	 redirect_to @course
+    if @course.update_attributes(course_params) 
+       redirect_to @course
     else
-      	render 'edit'
+        render 'edit'
     end
   end
 
@@ -62,5 +69,14 @@ class CoursesController < ApplicationController
       params.require(:course).permit(:name, :year, 
         :semester, :faculty, :number, :section, :instructor, :schedule, :description, :unit, :CourseUrl)
     end
+
+
+  def sort_column
+    Course.column_names.include?(params[:sort]) ? params[:sort] : "name"
+   end
+  
+   def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+   end
 
 end
