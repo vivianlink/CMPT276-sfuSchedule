16ff975ceb7bad06dc13a1479c31a773682a1e4d
jun <kzh36g@gmail.com>
2016-11-20 20:55:03 -0800

    fix search params

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,20 +1,16 @@
 class Course < ActiveRecord::Base
-	#belongs_to :faculty
-validates :terms_of_service, :acceptance => true
-
-  def self.search(faculty, number, year,semester, section, instructor, unit, designation)
-    #return scoped unless faculty.present? || number.present?
-
-    if faculty || number || year || semester || section || instructor || unit
-     
-     where(['faculty LIKE ? AND number LIKE ? AND year LIKE ? AND semester LIKE ? AND section LIKE ? AND instructor LIKE ? AND unit LIKE ? AND designation LIKE ?', 
-      "%#{faculty}%", "%#{number}%", "%#{year}%", "%#{semester}%", "%#{section}%", "%#{instructor}%", "%#{unit}%", "%#{designation}"])
+ 
+ 
+  def self.search(faculty, number, year,semester, unit, designation)
     
+    if faculty || number || year || semester || unit || designation #|| section || instructor || unit
+     
+     where(['faculty LIKE ? AND number LIKE ? AND year LIKE ? AND semester LIKE ? AND unit LIKE ? AND designation LIKE ?', 
+      "%#{faculty}%", "%#{number}%", "%#{year}%", "%#{semester}%", "%#{unit}%" ,"%#{designation}%"  ]) #, "%#{unit}%", "%#{designation}%"  ])
     else
       self.all
     end
   end
-	 
 
 
 end
