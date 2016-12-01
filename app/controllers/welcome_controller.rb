e346e18383553b0f4b0567bcbec747e61f8f84b6
vivianlink <vvivian@sfu.ca>
2016-12-01 00:41:43 -0800

    completed prof recommendation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,25 @@
 class WelcomeController < ApplicationController
 
+	class Prof
+	   def initialize(name, rating, commentsCount)
+	      @name=name
+	      @rating=rating
+	      @commentsCount=commentsCount
+	   end
+
+	   def commentsCount
+	   		@commentsCount
+	   end
+
+	   def rating
+	   		@rating
+	   end
+
+	   def name
+	   		@name
+	   end
+	end
+
 	def index
 
 		@sortedCourses = Course.order(viewCount: :desc)
@@ -11,7 +31,6 @@ class WelcomeController < ApplicationController
 				@nonEmptyView.push(course)
 			end
 		end 
-		
 
 		@sortedbyCart = Course.order(cartCount: :desc)
 		@nonEmptyCart = Array.new
@@ -20,7 +39,60 @@ class WelcomeController < ApplicationController
 				@nonEmptyCart.push(course)
 			end
 		end 
-	
+
+		@ratings = Rating.all
+
+		@prof_names = @ratings.uniq.pluck(:prof_name) 
+
+		@profs = Array.new
+
+		@prof_names.each do |prof_name|
+			@ratings = Rating.where(prof_name: prof_name)
+
+			@ratings_total = 0
+			@ratings_count = 0
+			@comments_count = 0
+
+			@ratings.each do |rating|
+				if rating.rating
+					@ratings_total += rating.rating
+					@ratings_count += 1
+				end
+
+				if rating.comment != ""
+					@comments_count += 1				
+				end
+			end
+
+			@rating_average = @ratings_total.to_f / @ratings_count
+			@rating_average = @rating_average.round(2)
+
+			@profs.push(Prof.new(prof_name, @rating_average, @comments_count))
+		end
+
+		@profs_by_comments_count = @profs.sort_by { |prof| [ prof.commentsCount ] }.reverse!
+
+		@profs_by_rating = @profs.clone
+
+		# remove profs with no comments
+		@profs_by_comments_count.delete_if do |prof|
+			if prof.commentsCount == 0
+				true
+			else
+				false
+			end
+		end
+
+		# remove profs with NaN rating
+		@profs_by_rating.delete_if do |prof|
+			if prof.rating.nan?
+				true
+			else
+				false
+			end
+		end
+
+		@profs_by_rating = @profs_by_rating.sort_by { |prof| [ prof.rating ] }.reverse!
 	end
 
 
