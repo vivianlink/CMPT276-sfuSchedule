5abb1c75c8a0c7fd8bc057fc0f21e77e93d3ee82
vpeng <vpeng@sfu.ca>
2016-12-05 10:08:40 -0800

    modified user test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,6 +25,22 @@ class UsersControllerTest < ActionController::TestCase
   test "should show course if user adds a course" do
     session[:user_name] = users(:one).username
     session[:user_id] = users(:one).id
+    get :show
+    assert_response :success
+    assert_select "th", {count: 0, text: "faculty"}
+    assert_select "th", {count: 0, text: "number"}
+    assert_select "th", {count: 0, text: "unit"}
+    assert_select "th", {count: 0, text: "designation"}
+    assert_select "th", {count: 0, text: "year"}
+    assert_select "th", {count: 0, text: "semester"}
+    assert_select "td", {count: 0, text: courses(:one).faculty}
+    assert_select "td", {count: 0, text: courses(:one).number}
+    assert_select "td", {count: 0, text: courses(:one).unit}
+    assert_select "td", {count: 0, text: courses(:one).designation}
+    assert_select "td", {count: 0, text: courses(:one).year}
+    assert_select "td", {count: 0, text: courses(:one).semester}
+    assert_select "td", {count: 0, text: "Remove"}
+    assert_select "td", {count: 0, text: "Show"}
     users(:one).course << courses(:one)
     get :show
     assert_response :success
@@ -40,11 +56,20 @@ class UsersControllerTest < ActionController::TestCase
     assert_select "td", {count: 1, text: courses(:one).designation}
     assert_select "td", {count: 1, text: courses(:one).year}
     assert_select "td", {count: 1, text: courses(:one).semester}
+    assert_select "td", {count: 1, text: "Remove"}
+    assert_select "td", {count: 1, text: "Show"}
   end
 
-  test "should show course if user adds a tutorial" do
+  test "should show tutorial if user adds a tutorial" do
     session[:user_name] = users(:one).username
     session[:user_id] = users(:one).id
+    get :show
+    assert_response :success
+    assert_select "th", {count: 0, text: "Name"}
+    assert_select "th", {count: 0, text: "Detail"}
+    assert_select "td", {count: 0, text: tutorials(:one).tutname}
+    assert_select "td", {count: 0, text: tutorials(:one).tutschedule}
+    assert_select "td", {count: 0, text: "Remove"}
     users(:one).tutorial << tutorials(:one)
     get :show
     assert_response :success
@@ -52,6 +77,8 @@ class UsersControllerTest < ActionController::TestCase
     assert_select "th", {count: 1, text: "Detail"}
     assert_select "td", {count: 1, text: tutorials(:one).tutname}
     assert_select "td", {count: 1, text: tutorials(:one).tutschedule}
+    assert_select "td", {count: 1, text: "Remove"}
+
   end
 
 end
