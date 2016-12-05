c7016edccbe02a0afe8695e98dfd3937c33884bb
zha34 <zha34@asb9838nu-a09.csil.sfu.ca>
2016-12-04 20:48:02 -0800

    add test for wel & course controllers

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -27,4 +27,21 @@ class WelcomeControllerTest < ActionController::TestCase
 	assert_select "a", {count: 1, text: "Become Admin"}
 	assert_select "a", {count: 1, text: session[:user_name]}
   end
+
+
+   test "search table" do
+    get :index
+    assert_response :success
+    assert_select "td", {count: 1, text: "Year:"}
+    assert_select "td", {count: 1, text: "Semester:"}
+    assert_select "td", {count: 1, text: "Faculty:"}
+    assert_select "td", {count: 1, text: "Number:"}
+    assert_select "td", {count: 1, text: "Unit:"}
+    assert_select "td", {count: 1, text: "Professor:"}
+    assert_select "td", {count: 1, text: "Designation:"}
+    assert_select "td", {count: 1, text: "Schedule Days:"}
+  end
+
+
+
 end
